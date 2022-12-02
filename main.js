0da05d8f1e813101bb21c2dfbec154fe1504f318
Ravshan-web <savurboyevr@gmail.com>
2022-12-02 20:06:33 +0500

    test-js

---
+++ main.js
@@ -0,0 +1,117 @@
+// ARRAY ichidan oxirgi elementni tanlab olish!
+let myPC = ['Monitor', 'System Block', 'Keyboard', 'Mouse', 'Graphic card', 'Processor'];
+console.log("Massivning oxirgi elementi: " + " '" + myPC[myPC.length-1] + "' ");
+console.log("Massivning birinchi elementi: " + " '" + myPC[0] + "' ");
+
+
+
+
+// Qudrat Abdurahimov - Java.exe misollaridan
+
+// Misol-1
+// let a =+prompt(`Kvadrat tomonlari nechi sm?`);
+// console.log("Kvadrat perimetri " + a*4 + "sm");
+
+
+// Misol-2
+// let a =+prompt(`Kvadrat tomonlari nechi sm?`);
+// console.log("Kvadrat yuzasi " + a**2 + " sm kv");
+
+
+// Misol - 3
+// let a =+prompt(`a ga qiymat bering`);
+// let b =+prompt(`b ga qiymat bering`);
+// console.log("Perimetr= " + (a+b)*2);
+// console.log("Yuzasi= " + a*b);
+
+// Misol - 4
+// let d =+prompt('Diametrga qiymat bering');
+// let pi = 3.14;
+// let l = d*pi;
+// console.log("Aylananing uzunligi " + l);
+
+// Misol - 5
+// let a =+prompt('Kubning tomoniga qiymat bering');
+// let v = a**3;
+// let s = 6*(a**2);
+// console.log('Hajmi = ' + v);
+// console.log('Yuzasi = ' + s);
+
+
+// Misol - 6
+// let a =+prompt('Paralelepipedning a tomoni');
+// let b =+prompt('Paralelepipedning b tomoni');
+// let c =+prompt('Paralelepipedning c tomoni');
+
+// let v = a*b*c;
+// let s = 2*(a*b+b*c+a*c);
+
+// console.log('Vajmi = '+ v);
+// console.log(`To'la sirti = ` + s);
+
+
+// Misol - 7
+// const pi = 3.14;
+// let r =+prompt('Doiraning radiusi tomoni');
+// let l = 2*pi*r;
+// let s = pi*(r**2);
+
+// console.log(`Uzunligi = ` + l);
+// console.log(`Yuzasi = ` + s);
+
+
+// Misol - 8
+// let a =+prompt('a = ');
+// let b =+prompt('b = ');
+// console.log(`O'rta arifmetik = ` + (a+b)/2);
+
+
+// Misol - 9
+// let a =+prompt('a = (musbat)');
+// let b =+prompt('b = (musbat)');
+// console.log(`O'rta geometrik = ` + Math.sqrt(a*b));
+
+// Misol - 10
+// let a =+prompt(`a = `);
+// let b =+prompt('b = ');
+
+// let sum = a+b;
+// let multiply = a*b;
+// console.log("Yig`indisi = " + sum);
+// console.log("Ko`paytmasi = " + multiply);
+// console.log("har birining moduli = " + Math.abs(a) + " va " + Math.abs(b));
+// console.log("har birining kvadrati = " + a**2 + " va " + b**2);
+
+
+// Misol - 12
+// let a =+prompt('Uchburchakning birinchi kateti = ');
+// let b =+prompt('Uchburchakning ikkinchi kateti = ');
+// let c = Math.sqrt(a**2 + b**2);
+// let p = a+b+c;
+// console.log('Gipotenuza = ' + c);
+// console.log('Perimetri = ' + p);
+
+//Misol - 13
+// let firstRadius =+prompt('R1 ga qiymat bering, R2 dan katta bo`lsin!');
+// let secondRadius =+prompt('R2 ga qiymat bering, R1 dan kichik bo`lsin!');
+// let pi = 3.14;
+// let firstSurface = firstRadius * pi;
+// let secondSurface  = secondRadius * pi;
+// console.log('Birinchi doira yuzi = ' + firstSurface);
+// console.log('ikkinchi doira yuzi = ' + secondSurface);
+// console.log('Ikkala doira yuzlarining ayirmasi = ' + (firstRadius - secondRadius)*pi );
+
+
+
+// Misol - 14 ~ Aylananing uzunligi berilgan uning radiusi va yuzini toping;
+// let length =+prompt('Aylananing uzunligi nechi bo`lsin?');
+// let radius = length/(3.14 * 2);
+// let surface = 3.14*radius**2;
+// let diameter = radius*2;
+// console.log('Diametri =' + diameter);
+// console.log("Radiusi = " + radius);
+// console.log('Yuzasi = ' + surface);
+
+
+
+
